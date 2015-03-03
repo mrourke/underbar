631b901fa1c3cdd533cc8dce4771a0e43c017bb8
Matthew Rourke <mrourke@gmail.com>
2015-03-03 01:54:08 -0500

    Completed .memoize()

--- src/underbar.js
+++ src/underbar.js
@@ -286,6 +286,15 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var computed = {};
+    return function(arg) {
+      if (arg in computed) {
+        return computed[arg];
+      } else {
+        computed[arg] = func.apply(this, arguments);
+        return computed[arg];
+      }
+    };
   };
 
   // Delays a function for the given number of milliseconds, and then calls
