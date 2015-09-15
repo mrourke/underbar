f0b6fb2d317fa8825d7a82970796299b5d23125a
Matthew Rourke <mrourke@gmail.com>
2015-09-15 11:12:56 -0400

    Clean up contains and every

--- src/underbar.js
+++ src/underbar.js
@@ -140,23 +140,17 @@
 
   // Determine if the array or object contains a given value (using `===`).
   _.contains = function (collection, target) {
-    // TIP: Many iteration problems can be most easily expressed in
-    // terms of reduce(). Here's a freebie to demonstrate!
     return _.reduce(collection, function (wasFound, item) {
-      if (wasFound) {
-        return true;
-      }
-      return item === target;
+      return wasFound || item === target;
     }, false);
   };
 
 
   // Determine whether all of the elements match a truth test.
   _.every = function (collection, iterator) {
-    // TIP: Try re-using reduce() here.
     if (iterator === undefined) { iterator = _.identity; }
-    return _.reduce(collection, function (sum, element) {
-      return (sum && Boolean(iterator(element)));
+    return _.reduce(collection, function (all, element) {
+      return all && Boolean(iterator(element));
     }, true);
   };
 
